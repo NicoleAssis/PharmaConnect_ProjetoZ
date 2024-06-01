66d30340f0182cd0386b3f4faa8df08fb84b77ab
Nicole de Assis <[nicassissiqueira@gmail.com](mailto:nicassissiqueira@gmail.com)>
2024-06-01 15:47:23 -0300

    Adicionando Classe Cliente

---
+++ TESTE GUNA/projeto.model/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTE_GUNA.projeto.model
+{
+    public class Cliente
+    {
+        public int cpf_cnpj_cliente { get; set; }
+        public string nome_cliente { get; set; }
+        public string email_cliente { get; set; }
+        public int telefone_cliente { get; set; }
+        public string celular_cliente { get; set; }
+        public string cep_cliente { get; set; }
+        public string endereco_cliente { get; set; }
+        public string numero_cliente { get; set; }
+        public string complemento_cliente { get; set; }
+        public string bairro_cliente { get; set; }
+        public string cidade_cliente { get; set; }
+        public string estado_cliente { get; set; }
+        public string senha_cliente { get; set; }
+        public int nivel_acesso { get; set; }
+
+    }
+}
