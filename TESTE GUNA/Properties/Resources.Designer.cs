a1d235ba444c60662ca5ac2a10c233e81e74b081
Nicole de Assis <nicassissiqueira@gmail.com>
2024-06-19 21:52:44 -0300

    Continuar comprando ou add ao carrinho

--- TESTE GUNA/Properties/Resources.Designer.cs
+++ TESTE GUNA/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace TESTE_GUNA.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap azulclean {
+            get {
+                object obj = ResourceManager.GetObject("azulclean", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap iconAlterarProduto {
             get {
                 object obj = ResourceManager.GetObject("iconAlterarProduto", resourceCulture);
