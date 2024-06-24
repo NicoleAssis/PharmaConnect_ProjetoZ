0b626bdd601a9f31ca2c2354d7205bbc87cd8b1d
Larissa Camyle <larissacamylejob@gmail.com>
2024-06-24 13:47:05 -0300

    .

---
+++ TESTE GUNA/projeto.view/AdmFrmMenu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TESTE_GUNA.projeto.dao;
+
+namespace TESTE_GUNA.projeto.view
+{
+    public partial class AdmFrmMenu : Form
+    {
+        public AdmFrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            //button fechar o programa (X)
+            Environment.Exit(0);
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            //volta para a tela e login
+            FrmLogin telaLogin = new FrmLogin();
+            this.Close();
+            telaLogin.Show();
+        }
+
+        private void btnProdutos_Click(object sender, EventArgs e)
+        {
+            
+            AdmFrmProdutos telaProdutos = new AdmFrmProdutos();
+            this.Close();
+            telaProdutos.Show();
+        }
+
+        private void txtPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            string nome = "%" + txtPesquisa.Text + "%";
+            ProdutoDAO dao = new ProdutoDAO();
+
+            DataGridViewVendas.DataSource = dao.LIstarProdutosPorNome(nome);
+        }
+
+        private void txtPesquisa_Click(object sender, EventArgs e)
+        {
+            txtPesquisa.Text = "";
+        }
+
+        private void btnCadastrarAdm_Click(object sender, EventArgs e)
+        {
+            AdmFrmCadastroAdministrador telaCadastro = new AdmFrmCadastroAdministrador();
+            this.Close();
+            telaCadastro.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridViewVendas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnPerfil_Click(object sender, EventArgs e)
+        {
+            FrmADMPerfil telaPerfil = new FrmADMPerfil();
+            this.Close();
+            telaPerfil.Show();
+        }
+
+        private void btnVendas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdmFrmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
